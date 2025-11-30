c0d850ba64a014e258e5bbb654cc946884fa7470
drilathedev <webdevdrila@gmail.com>
2025-11-30 14:37:51 +0100

    fox

--- firebase.js
+++ firebase.js
@@ -3,14 +3,15 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// Use environment variables from .env
 const firebaseConfig = {
-  apiKey: "AIzaSyCJcbWJqAYDVhaKqOHDBOlCgDRPE8KGiqU",
-  authDomain: "prona360-fdffd.firebaseapp.com",
-  projectId: "prona360-fdffd",
-  storageBucket: "prona360d.firebasestorage.app",
-  messagingSenderId: "13070047414",
-  appId: "1:13070047414:web:a5fc2afd1153800f1fae17",
-  measurementId: "G-HDWE7Q1WJD"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
